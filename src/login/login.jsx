7f31e686d136b4661f5b64f70bdf76b3218170a6
ryanvance74 <58126311+ryanvance74@users.noreply.github.com>
2026-02-12 17:13:27 -0700

    removed form functionality in order to stay within SPA

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -5,7 +5,7 @@ export function Login() {
   return (
     <main className="container-fluid flex-grow-1 d-flex flex-column align-items-center">
       <h1>Welcome to MusicalRankings, the site where you can rank musicals!</h1>
-      <form method="get" action="rank.html">
+      <form onSubmit={(e) => e.preventDefault()}>
         <div className="input-group mb-3">
           <span className="input-group-text">@</span>
           <input className="form-control" type="text" placeholder="your@email.com" />
