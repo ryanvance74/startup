48cec7c8cad966bdd8fb040a8c3ea1f4dd7a470e
ryanvance74 <58126311+ryanvance74@users.noreply.github.com>
2026-02-12 17:19:47 -0700

    implemented rank and friends jsx components

---
+++ src/friends/friends.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './friends.css'
+export function Friends() {
+  return (
+    <main className="container-fluid flex-grow-1 d-flex flex-column justify-content-between">
+        <div className="row">
+            <div className="col-md-6">
+                <h4>Send a Friend Request:</h4>
+                    <div className="input-group mb-6">
+                    <span className="input-group-text">@</span>
+                    <input className="form-control" type="text" placeholder="your@email.com" />
+                    </div>
+                    <div>
+                        <Button variant='primary' onClick={() => {}}>
+                        Send Friend Request
+                        </Button>
+                    </div>
+            </div>
+            <div className="col-md-6">
+                <h4 className="text-center" id="table-title"> Friends </h4>
+                <table className="table table-success table-striped-columns">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Musicals Rated</th>
+                        <th>Friends Since</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td> Tim</td>
+                        <td>10</td>
+                        <td>May 20, 2021</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Ada</td>
+                        <td>29</td>
+                        <td>June 2, 2021</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Gunter Spears</td>
+                        <td>7</td>
+                        <td>July 3, 2020</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+                
+        </div>
+        
+    </main>
+  );
+}
---
+++ src/rank/rank.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './friends.css'
+export function Friends() {
+  return (
+    <main className="container-fluid flex-grow-1 d-flex flex-column align-items-left">
+      <div className="row">
+        <div className="col-md-3">
+            <h4 className="area-header">Notifications:</h4>
+      <div className="notification-col">
+        <ul> 
+            <div><span className="notification">Your friend Tim rated Les Mis 3 out of 5</span></div>
+            <div><span className="notification">Ada sent you a friend request</span></div>
+        </ul>
+      </div>
+      <br />
+
+      <div>
+        <h4>Musicals:</h4>
+        <span id="musical-name">Hamilton</span>
+      </div>
+      <div className="input-group mb-3">
+        <label className="input-group-text" for="count">How many stars is this musical?</label>
+        <input className="form-control" type="text" id="count" value="--" readonly />
+      </div>
+      <br />
+      <div>
+        <Button variant='primary' onClick={() => {}}>
+                        Submit Rating
+                        </Button>
+      </div>
+      <br />
+        </div>
+      </div>
+      
+    </main>
+  );
+}
