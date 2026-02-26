fb96f0b3cec2057f1e6352939c070b7665b80fac
ryanvance74 <58126311+ryanvance74@users.noreply.github.com>
2026-02-26 13:30:08 -0700

    added reactivity to showings page

--- src/showings/showings.jsx
+++ src/showings/showings.jsx
@@ -3,6 +3,27 @@ import './showings.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 export function Showings() {
+    const [musicals, setMusicals] = useState([]);
+    const [loadingMessage, setLoadingMessage] = useState(false);
+    const mockAPIData = [
+        {id: 1, name: 'Les Mis', showings_today: 1, price: 140},
+        {id: 2, name: 'Hamilton', showings_today: 2, price: 250},
+        {id: 3, name: 'Wicked', showings_today: 4, price: 180},
+        {id: 4, name: 'Phantom of the Opera', showings_today: 0, price: 'N/A'}
+    ]
+    const fetchRequest = () => {
+        setLoadingMessage(true);
+        setTimeout(() => {
+            const idx = Math.floor(Math.random() * 3)
+            const new_val = Math.floor(Math.random() * 500)
+            mockAPIData[idx].price = new_val
+            setMusicals(mockAPIData)
+            setLoadingMessage(false)
+        }, 3000)
+    }
+    useEffect(() => {
+        fetchRequest();
+    }, [])
   return (
     <main className="container-fluid flex-grow-1 d-flex flex-column justify-content-between">
         <div className="row">
@@ -18,31 +39,25 @@ export function Showings() {
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td> Les Mis</td>
-                        <td>1</td>
-                        <td>$120</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Hamilton</td>
-                        <td>3</td>
-                        <td>$400</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Wicked</td>
-                        <td>2</td>
-                        <td>$210</td>
-                    </tr>
+                    {musicals.map((musical, i) => (
+                        <tr key={i}>
+                            <td>{i + 1}</td> 
+                            <td>{musical.name}</td>
+                            <td>{musical.showings_today}</td>
+                            <td>{musical.price}</td>
+                        </tr>
+                    ))}
                     </tbody>
                 </table>
                 <div>
-                    <Button variant='primary' onClick={() => {}}>
+                    <Button variant='primary' onClick={fetchRequest}>
                         Refresh Listings
                         </Button>
                     </div>
+                {loading && <div>
+                    Loading latest showings from the West End!
+                </div>
+                    }  
             </div>
         </div>
         
