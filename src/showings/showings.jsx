ebeb806e4607911182c270fec76ccbd2c11ce2ae
ryanvance74 <58126311+ryanvance74@users.noreply.github.com>
2026-03-12 15:15:35 -0600

    implemented API request and removed mock API data

--- src/showings/showings.jsx
+++ src/showings/showings.jsx
@@ -8,12 +8,7 @@ const API_KEY = BMdcxMfnXQJYhVAo8nxMnNNb0G1ZOrlv;
 export function Showings() {
     const [musicals, setMusicals] = useState([]);
     const [loadingMessage, setLoadingMessage] = useState(false);
-    const mockAPIData = [
-        {id: 1, name: 'Les Mis', showings_today: 1, price: 140},
-        {id: 2, name: 'Hamilton', showings_today: 2, price: 250},
-        {id: 3, name: 'Wicked', showings_today: 4, price: 180},
-        {id: 4, name: 'Phantom of the Opera', showings_today: 0, price: 'N/A'}
-    ]
+    
     const fetchRequest = async () => {
         setLoadingMessage(true);
         const url = new URL(`https://app.ticketmaster.com/discovery/v2/events.json`)
@@ -21,20 +16,21 @@ export function Showings() {
         url.searchParams.set('city', 'New York');
         url.searchParams.set('stateCode', 'NY');
         url.searchParams.set('apikey', API_KEY);
-        
-        const res = await fetch(url.toString());
-        const body = await res.json();
-        
-        setTimeout(() => {
-            const idx = Math.floor(Math.random() * 3)
-            const new_val = Math.floor(Math.random() * 450 + 70)
-            const showings_val = Math.floor(Math.random() * 3 + 1)
-            const updatedData = [...mockAPIData]
-            updatedData[idx].price = new_val
-            updatedData[idx].showings_today = showings_val
-            setMusicals(mockAPIData)
-            setLoadingMessage(false)
-        }, 3000)
+        try {
+            const res = await fetch(url.toString());
+            const body = await res.json();
+            const events = body._embedded?.events || []
+            const processed = events.map((event) => ({
+                name: event.name,
+                location: event._embedded.venues[0].name,
+                date: event.dates.start.dateTime
+            }))
+            setMusicals(processed)
+        } catch (err) {
+            console.error("Failed to fetch TicketMaster API: ", err);
+        } finally {
+            setLoadingMessage(false);
+        }
     }
     useEffect(() => {
         fetchRequest();
