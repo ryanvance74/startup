c127388e06192ef678e00c090c1ce3365cdb9a4f
ryanvance74 <58126311+ryanvance74@users.noreply.github.com>
2026-03-12 15:29:49 -0600

    added the rest of the default backend logic from simon

--- service/index.js
+++ service/index.js
@@ -5,7 +5,7 @@ const uuid = require('uuid');
 const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
-
+const authCookieName = 'token';
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
@@ -23,6 +23,25 @@ apiRouter.post('/auth/create', async (req, res) => {
     res.send({ email: user.email });
   }
 });
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
 
 apiRouter.post('/auth/login', async (req, res) => {
   const user = await findUser('email', req.body.email);
@@ -55,6 +74,14 @@ const verifyAuth = async (req, res, next) => {
   }
 };
 
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
 const http = require('http');
 const server = http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -65,3 +92,13 @@ const server = http.createServer(function (req, res) {
 server.listen(8080, () => {
   console.log(`Web service listening on port 8080`);
 });
+
+
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
