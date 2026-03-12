fda84446a0a2f9fec2c5809f151ae5cabc0581a2
ryanvance74 <58126311+ryanvance74@users.noreply.github.com>
2026-03-12 12:10:07 -0600

    added base server code

--- service/index.js
+++ service/index.js
@@ -1,2 +1,15 @@
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
-app.use(express.static('public'));
+
+
+const http = require('http');
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(`<h1>Hello Node.js! [${req.method}] ${req.url}</h1>`);
+  res.end();
+});
+
+server.listen(8080, () => {
+  console.log(`Web service listening on port 8080`);
+});
+
+// const port = process.argv.length > 2 ? process.argv[2] : 4000;
+// app.use(express.static('public'));
