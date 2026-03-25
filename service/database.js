3fdb200dc350048aecc884c621870f85be8f2d37
ryanvance74 <58126311+ryanvance74@users.noreply.github.com>
2026-03-24 18:11:15 -0600

    added async connection test function

--- service/database.js
+++ service/database.js
@@ -4,3 +4,13 @@ const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('simon');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
