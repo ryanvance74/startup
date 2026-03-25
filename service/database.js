1ffa0af875e995ab8d5efff6910e6b65b74e462a
ryanvance74 <58126311+ryanvance74@users.noreply.github.com>
2026-03-24 18:16:03 -0600

    fixed db client name and added user collection to hold auth credentials

--- service/database.js
+++ service/database.js
@@ -3,7 +3,8 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
+const db = client.db('startup');
+const userCollection = db.collection('user');
 
 (async function testConnection() {
   try {
