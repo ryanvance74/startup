25ca09219001bc0fea1d7d2b2831704b029a43ee
ryanvance74 <58126311+ryanvance74@users.noreply.github.com>
2026-04-07 19:30:43 -0600

    added a rankingEvent enum for extensibility

--- src/rank/rankingNotifier.js
+++ src/rank/rankingNotifier.js
@@ -9,4 +9,22 @@ class EventMessage {
 class RankingEventNotifier {
     events = [];
     handlers = [];
+
+    constructor() {
+        let port = window.location.port;
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+        this.socket.onopen = (event) => {
+          this.receiveEvent(new EventMessage('Musical Rankings', GameEvent.System, { msg: 'connected' }));
+        };
+        this.socket.onclose = (event) => {
+          this.receiveEvent(new EventMessage('Musical Rankings', GameEvent.System, { msg: 'disconnected' }));
+        };
+        this.socket.onmessage = async (msg) => {
+          try {
+            const event = JSON.parse(await msg.data.text());
+            this.receiveEvent(event);
+          } catch {}
+        };
+      }
 }
